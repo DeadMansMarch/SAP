b334656622a0c3e336913b45eda761ad5867238e
DeadMansMarch <thedeadmansmarch@gmail.com>
2017-04-03 17:05:39 -0400

    Added switches.

--- SAP/VirtualMachine.swift
+++ SAP/VirtualMachine.swift
@@ -13,10 +13,40 @@ class VirtualMachine{
     //Dis rih here a virtual machene.
     private(set) var AccessMem = [Int](); //'Memory'.
     private(set) var AccessReg = [Int](); //Registers.
-    private(set) var SpReg = ["PRGM":0,"CMPR":0,"STCK":0]; //Special Registers.
+    private(set) var SpReg = ["PGRM":0,"CMPR":0,"STCK":0]; //Special Registers.
     
     func setMemoryLength(Length L:Int){ //Resets memory to certain size.
         AccessMem = [Int](repeating:0,count:8);
+    }
+    
+    func cmdSwitch(With c:Int){ //Get command from Int.
+        switch(c){
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 8:  //movemr.
+                break;
+            case 12: //addir.
+                break;
+            case 13: //addrr.
+                break;
+            case 34: //cmprr.
+                break;
+            case 45: //outcr.
+                break;
+            case 49: //printi
+                break;
+            case 55: //outs.
+                break;
+            case 57: //jumpne
+                SpReg["PGRM"] = AccessMem[SpReg["PGRM"]! + 1]; //Set PGRM counter to location in memory : PGRM + 1
+                break;
+            default:
+                break;
+        }
     }
     
 }
