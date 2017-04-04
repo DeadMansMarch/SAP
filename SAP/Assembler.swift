63c56fa62bcba3618254b329d484070b60b94bd5
DeadMansMarch <thedeadmansmarch@gmail.com>
2017-04-04 08:55:03 -0400

    Starting assembler.

---
+++ SAP/Assembler.swift
@@ -0,0 +1,24 @@
+//
+//  Assembler.swift
+//  SAP
+//
+//  Created by Liam Pierce on 4/4/17.
+//  Copyright © 2017 Liam Pierce. All rights reserved.
+//
+
+import Foundation
+
+//Crude attempt at an assembler.
+
+class Assembler{
+    
+    private(set) var program:String? = "";
+    
+    func load(Program:String){
+        self.program = Program;
+    }
+    
+    func Assemble(){
+        
+    }
+}
