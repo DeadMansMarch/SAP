1e7a2c995cd5edb7214bfe1e409a3ac81baa6009
Charlie Mirabile <charliemirabile@charlies-macbook-air-2.local>
2017-04-04 16:45:51 -0400

    added some opcodes and hardcoded number of params and a silly program

--- SAP/VirtualMachine.swift
+++ SAP/VirtualMachine.swift
@@ -13,14 +13,62 @@ class VirtualMachine{
     //Dis rih here a virtual machene.
     let NumOfParamsNeeded = [
         0:0,
+        1:1,
+        2:1,
+        3:1,
+        4:2,
+        5:2,
         6:2,
+        7:2,
         8:2,
+        9:2,
+        10:2,
+        11:3,
         12:2,
         13:2,
+        14:2,
+        15:2,
+        16:2,
+        17:2,
+        18:2,
+        19:2,
+        20:2,
+        21:2,
+        22:2,
+        23:2,
+        24:2,
+        25:2,
+        26:2,
+        27:2,
+        28:1,
+        29:2,
+        30:2,
+        31:2,
+        32:2,
+        33:2,
         34:2,
+        35:2,
+        36:1,
+        37:1,
+        38:1,
+        39:1,
+        40:0,
+        41:1,
+        42:1,
+        43:1,
+        44:1,
         45:1,
+        46:1,
+        47:2,
+        48:2,
         49:1,
+        50:1,
+        51:2,
+        52:0,
+        53:2,
+        54:2,
         55:1,
+        56:0,
         57:1
     ];
     private(set) var RAMArray = [Int](); //'Memory'.
@@ -34,11 +82,10 @@ class VirtualMachine{
     }
     
     func loadMem(FullMem Data:[Int]){
-        self.setMemoryLength(Length: Data[0]); //Set memory length.
+        self.setMemoryLength(Length: 1000); //Set memory length.
         SpecialRegisters["PGRM"] = Data[1];               //Set program counter to initial position.
-        
-        for i in 0..<Data[0]{
-            RAMArray[i] = Data[i + 2];        //Fill Memory.
+        for i in 2..<Data.count{
+            RAMArray[i-2] = Data[i];        //Fill Memory.
         }
     }
     func checkRegister(_ rNum:Int)->Bool{
@@ -87,7 +134,7 @@ class VirtualMachine{
             return false;
         }
         guard let NumRAMLocationsInvolved = NumOfParamsNeeded[c] else{
-            print("FATAL ERROR: COMMAND NOT RECOGNIZED");
+            print("FATAL ERROR: COMMAND NOT RECOGNIZED: \(c) PC: \(SpecialRegisters["PGRM"]!)");
             return false;
         }
         let Paremeters = movePC_GetParams(Amount:NumRAMLocationsInvolved);
@@ -138,6 +185,24 @@ class VirtualMachine{
                 }
                 RegistersArray[Paremeters[1]] = RegistersArray[Paremeters[1]] + RegistersArray[Paremeters[0]];
                 break;
+            case 28://jmp
+                guard checkMemoryLocation(Paremeters[0]) else {
+                    print("FATAL ERROR ILLEGAL MEMORY LOCATION: \(Paremeters[0]) PC: \(SpecialRegisters["PGRM"]!)")
+                    break;
+                }
+                SpecialRegisters["PGRM"]=Paremeters[0]
+            case 33:
+                guard checkRegister(Paremeters[1]) else{
+                    print("FATAL ERROR: ILLEGAL REGISTER #: \(Paremeters[1]) PC: \(SpecialRegisters["PGRM"]!)")
+                    break;
+                }
+                if Paremeters[0] > RegistersArray[Paremeters[1]]{
+                    SpecialRegisters["CMPR"] = 2;
+                }else if Paremeters[0] < RegistersArray[Paremeters[1]]{
+                    SpecialRegisters["CMPR"] = 0;
+                }else{
+                    SpecialRegisters["CMPR"] = 1;
+                }
             case 34: //cmprr.
                 guard checkRegister(Paremeters[0]) else{
                     print("FATAL ERROR: ILLEGAL REGISTER #: \(Paremeters[0]) PC: \(SpecialRegisters["PGRM"]!)")
--- SAP/main.swift
+++ SAP/main.swift
@@ -9,10 +9,14 @@
 import Foundation
 
 // I have a save file class we can use for read/write stuff, but for now:
-let MemeDoublerProgram = [79,43,0,20,10,26,65,32,80,114,111,103,114,97,109,32,84,111,32,80,114,105,110,116,32,68,111,117,98,108,101,115,12,32,68,111,117,98,108,101,100,32,105,115,32,8,0,8,8,1,9,8,2,0,55,3,45,0,6,8,1,13,8,1,49,8,55,30,49,1,45,0,34,8,9,12,1,8,57,56,0]
-
-print(MemeDoublerProgram.count);
+let MemeDoublerProgram = [79,43,0,30,10,26,65,32,80,114,111,103,114,97,109,32,84,111,32,80,114,105,110,116,32,68,111,117,98,108,101,115,12,32,68,111,117,98,108,101,100,32,105,115,32,8,0,8,8,1,9,8,2,0,55,3,45,0,6,8,1,13,8,1,49,8,55,30,49,1,45,0,34,8,9,12,1,8,57,56,0]
+let shitProgram = [58, 50, 45, 84, 104, 101, 32, 77, 105, 116, 111, 99, 104, 111, 110, 100, 114, 105, 97, 32, 73, 115, 32, 116, 104, 101, 32, 80, 111, 119, 101, 114, 104, 111, 117, 115, 101, 32, 111, 102, 32, 116, 104, 101, 32, 67, 101, 108, 108, 10, 55, 0, 33, 10, 0, 12, 1, 0, 57, 48]
+//print(shitProgram.count)
+//print(MemeDoublerProgram.count)
+//print("The Mitochondria Is the Powerhouse of the Cell\n".characters.map{String($0).unicodeScalars}.map{$0[$0.startIndex].value})
+//print("\("The Mitochondria Is the Powerhouse of the Cell\n".characters.count)\n\n\n\n")
+//print(MemeDoublerProgram.count);
 var VM = VirtualMachine();
 
-VM.loadMem(FullMem: MemeDoublerProgram);
+VM.loadMem(FullMem: shitProgram);
 VM.Execute()
